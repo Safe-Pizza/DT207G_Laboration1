6dfd5f478d2ff90dd55d585fb967171f8f0a4d3b
Hanna Lindkvist <hali2507@student.miun.se>
2026-03-30 15:42:12 +0200

    skriver create table

--- install.js
+++ install.js
@@ -1,2 +1,16 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./db/courses.db");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE course (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        course_code TEXT NOT NULL,
+        course_name TEXT NOT NULL,
+        course_progression TEXT NOT NULL,
+        course_syllabus TEXT NOT NULL,
+        user_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        )`);
+});
+
+db.close();
