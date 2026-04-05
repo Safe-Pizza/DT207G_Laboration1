c868cf16f765a6da23beb162a4a8802f0fd270af
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-05 14:46:02 +0200

    ändrar port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 const bodyParser = require("body-parser");
 
 //Anslutning databas
